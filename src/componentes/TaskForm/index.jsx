5a14f47f24f177fffc9eba791adf457ee672386a
mruiz <mruiz@rmcuantica.com>
2023-11-22 02:23:41 -0300

    "Se da formato al archivo"

--- src/componentes/TaskForm/index.jsx
+++ src/componentes/TaskForm/index.jsx
@@ -1,29 +1,41 @@
 import Dialog from "@mui/material/Dialog";
 import { useState } from "react";
 
-export default function TaskForm(props){
-    const {onCerrar, tarea, agregarTarea, editarTarea}=props
-    const [miTarea, setMiTarea] = useState(tarea??{id:null,nombre:"Nueva tarea!", completado: false})
-   
-    //bien podría ser un componente en sí.
-    const inputTarea=(propiedad)=>{
-        return <div key={"PROP_"+propiedad}>
-            <label>{`${propiedad}: `}</label>
-            <input onChange={(e)=>{
-                setMiTarea({...miTarea, [propiedad]:e.target.value})
-            }} value={miTarea[propiedad]}></input>
-        </div>
-    }
-    const onSubmitForm=(e)=>{
-        miTarea.id?editarTarea(miTarea):agregarTarea(miTarea); onCerrar()
-    }
-    const campos=["nombre"]
-    return <Dialog open={true} onClose={onCerrar}>
-        <form onSubmit={onSubmitForm}>
-            {campos.map((x)=>inputTarea(x))}
-            <input type="submit" value={miTarea.id?"Modificar":"Añadir"}></input>
-            <input type="button" value="Cerrar" onClick={onCerrar}></input>
-        </form>
+export default function TaskForm(props) {
+  const { onCerrar, tarea, agregarTarea, editarTarea } = props;
+  const [miTarea, setMiTarea] = useState(
+    tarea ?? { id: null, nombre: "Nueva tarea!", completado: false }
+  );
+
+  //bien podría ser un componente en sí.
+  const inputTarea = (propiedad) => {
+    return (
+      <div key={"PROP_" + propiedad}>
+        <label>{`${propiedad}: `}</label>
+        <input
+          onChange={(e) => {
+            setMiTarea({ ...miTarea, [propiedad]: e.target.value });
+          }}
+          value={miTarea[propiedad]}
+        ></input>
+      </div>
+    );
+  };
+  const onSubmitForm = (e) => {
+    miTarea.id ? editarTarea(miTarea) : agregarTarea(miTarea);
+    onCerrar();
+  };
+  const campos = ["nombre"];
+  return (
+    <Dialog open={true} onClose={onCerrar}>
+      <form onSubmit={onSubmitForm}>
+        {campos.map((x) => inputTarea(x))}
+        <input
+          type="submit"
+          value={miTarea.id ? "Modificar" : "Añadir"}
+        ></input>
+        <input type="button" value="Cerrar" onClick={onCerrar}></input>
+      </form>
     </Dialog>
   );
 }
