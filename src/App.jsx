b5cbb9bc5f9189cccccda8a1738d2382f26b3729
mruiz <mruiz@rmcuantica.com>
2023-11-22 00:31:03 -0300

    "Se brinda la funcionalidad de cambiar el estado de completado"

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TaskForm from './componentes/TaskForm'
-import TaskItem from './componentes/TaskItem'
+import { useState } from "react";
+import "./App.css";
+import TaskItem from "./componentes/TaskItem";
 //Esta app es solo para testeo del componente TaskForm hasta que estén implementadas sus dependencias e interfaces
 function App() {
-  const [tasks,setTasks]=useState([
-    {id:"TSK_1",nombre:"Tarea del dia - TaskForm", completado: false},
-    {id:"TSK_2",nombre:"Tomar un cafe", completado: true},
-  ])
-  const [idCounter, setIdCounter] = useState(tasks.length+1)
-  const [mostrarForm, setMostrarForm]=useState(false)
-  const [tareaSeleccionada, setTareaSeleccionada]=useState(null)
 
-  const nuevoId=()=>{
+
+  const [tasks, setTasks] = useState([
+    { id: "TSK_1", nombre: "Tarea del dia - TaskForm", completado: false },
+    { id: "TSK_2", nombre: "Tomar un cafe", completado: true },
+  ]);
+
+  const [idCounter, setIdCounter] = useState(tasks.length + 1);
+
+  const [mostrarForm, setMostrarForm] = useState(false);
+
+  const [tareaSeleccionada, setTareaSeleccionada] = useState(null);
+
+  const nuevoId = () => {
     //La forma mas simple de generar un id unico, es siempre incrementarlo, asi nunca se repite
-    setIdCounter(idCounter+1)
-    return idCounter
-  }
+    setIdCounter(idCounter + 1);
+    return idCounter;
+  };
 
-  const eliminarTarea=(tareaEliminada)=>{
-    //filtrar la tarea a eliminar segun id
-    setTasks(tasks.filter((tarea)=>{
-      return (tareaEliminada.id!=tarea.id)
-    }))
-  }
-  const editarTarea=(tareaEditada)=>{
-    //reemplazar la tarea en tasks segun id
-    setTasks(tasks.map((tarea)=>{
-      return (tareaEditada.id!=tarea.id)?tarea:tareaEditada
-    }))
-  }
-  const agregarTarea=(nuevaTarea)=>{
-    nuevaTarea.id=`TSK_${nuevoId()}`
-    setTasks([nuevaTarea, ...tasks])
-  }
+
+  const agregarTarea = (nuevaTarea) => {
+    nuevaTarea.id = `TSK_${nuevoId()}`;
+    setTasks([nuevaTarea, ...tasks]);
+  };
 
   return (
     <>
       <h1>mis Tareas</h1>
 
-      <button onClick={()=>{
-        setTareaSeleccionada(null)
-        setMostrarForm(!mostrarForm)
-        }}>Agregar Tarea</button>
-      
+      <button
+        onClick={() => {
+          setTareaSeleccionada(null);
+          setMostrarForm(!mostrarForm);
+        }}
+      >
+        Agregar Tarea
+      </button>
+
       {/* Renderizar una lista de TaskItem*/}
 
       <TaskItem/>
-      
-      <ul>
-        {tasks.map((x)=>{
-          return <li key={x.id}>
-            <div>[{x.id}] {x.nombre}</div>
-            <button onClick={()=>{
-              setTareaSeleccionada(x)
-              setMostrarForm(true)
-            }}>editar</button>
-            <button onClick={()=>{
-              eliminarTarea(x)
-            }}>Borrar</button>
-          </li>
-        })}
-      </ul>
 
-      <div>
-        {/* Renderizar el TaskForm. Mantener el condicional por fuera del componente (tarea se copia durante el primer renderizado, hasta que se cierra)*/}
-        {mostrarForm&&<TaskForm onCerrar={()=>setMostrarForm(false)} tarea={tareaSeleccionada} agregarTarea={agregarTarea} editarTarea={editarTarea}/>}
-      </div>
+      {/* <ul>
+        {tasks.map((x) => {
+          return (
+            <li key={x.id}>
+              <div>
+                [{x.id}] {x.nombre}
+              </div>
+              <button
+                onClick={() => {
+                  setTareaSeleccionada(x);
+                  setMostrarForm(true);
+                }}
+              >
+                editar
+              </button>
+              <button
+                onClick={() => {
+                  eliminarTarea(x);
+                }}
+              >
+                Borrar
+              </button>
+            </li>
+          );
+        })}
+      </ul> */}
+
+   
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componentes/TaskItem/index.jsx
+++ src/componentes/TaskItem/index.jsx
@@ -6,6 +6,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { useState } from "react";
+import TaskForm from "../TaskForm";
 
 export default function TaskItem() {
   const [tasks, setTasks] = useState([
@@ -13,64 +14,59 @@ export default function TaskItem() {
     { id: "TSK_2", nombre: "Tomar un cafe", completado: true },
   ]);
 
-//   const tareaRealizada = (tareaTerminada) => {
-//     setTasks(
-//       tasks.filter((tarea) => {
-//         return (tarea.id = tareaTerminada.id
-//           ? (tarea.completado = true)
-//           : (tarea.completado = false));
-//       })
-//     );
-//   };
-//   const eliminarTarea = (tareaEliminada) => {
-//     //filtrar la tarea a eliminar segun id
-//     setTasks(
-//       tasks.filter((tarea) => {
-//         return tareaEliminada.id != tarea.id;
-//       })
-//     );
-//   };
-//   const editarTarea = (tareaEditada) => {
-//     //reemplazar la tarea en tasks segun id
-//     setTasks(
-//       tasks.map((tarea) => {
-//         return tareaEditada.id != tarea.id ? tarea : tareaEditada;
-//       })
-//     );
-//   };
+  const [mostrarForm, setMostrarForm] = useState(false);
+
+const onChangeCompletado = (tareaId) => {
+  setTasks((prevTasks) =>
+    prevTasks.map((tarea) =>
+      tarea.id === tareaId ? { ...tarea, completado: !tarea.completado } : tarea
+    )
+  );
+};
+
+  const eliminarTarea = (tareaEliminada) => {
+    //filtrar la tarea a eliminar segun id
+    setTasks(
+      tasks.filter((tarea) => {
+        return tareaEliminada.id != tarea.id;
+      })
+    );
+  };
+  const editarTarea = (tareaEditada) => {
+    //reemplazar la tarea en tasks segun id
+    setTasks(
+      tasks.map((tarea) => {
+        return tareaEditada.id != tarea.id ? tarea : tareaEditada;
+      })
+    );
+  };
 
   return (
     <>
       {tasks.length > 0 ? (
-        <div>
-          {tasks.map((x) => {
-            return (
-              <Box lg={{ flexGrow: 1 }} class="box-container" key={x.id}>
-                <Grid container spacing={0}>
-                  <Grid
-                    container
-                    lg={2}
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                  >
+        <div className="container">
+          {tasks.map((x) => (
+            <div key={x.id} className="container-box">
+              <Box lg={{ flexGrow: 1 }} className="box-container">
+                <Grid
+                  container
+                  spacing={1}
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Grid item lg={2}>
                     <Button
                       size="small"
                       variant="contained"
                       color={x.completado ? "success" : "primary"}
-                    //   onClick={tareaRealizada}
+                      onClick={()=>onChangeCompletado(x.id)}
                     >
                       {x.completado ? "Realizada" : "No Realizada"}
                     </Button>
                   </Grid>
 
-                  <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    lg={8}
-                  >
+                  <Grid item lg={6}>
                     <Typography
                       sx={{ fontSize: 24 }}
                       color="text.secondary"
@@ -80,43 +76,54 @@ export default function TaskItem() {
                     </Typography>
                   </Grid>
 
-                  <Grid
-                    container
-                    lg={2}
-                    spacing={1}
-                    direction="row"
-                    justifyContent="flex-end"
-                    alignItems="center"
-                  >
-                    <Grid item lg={12}>
-                      <Button size="small" variant="contained" color="error">
-                        <DeleteForeverIcon
-                        //   onClick={eliminarTarea(x)}
-                        ></DeleteForeverIcon>
-                      </Button>
-                    </Grid>
-
-                    <Grid item lg={12}>
-                      <Button size="small" variant="contained" color="warning">
-                        <EditIcon 
-                        // onClick={editarTarea(x)}
-                        ></EditIcon>
-                      </Button>
+                  <Grid item lg={2}>
+                    <Grid
+                      container
+                      spacing={1}
+                      direction="column"
+                      alignItems="center"
+                    >
+                      <Grid item lg={2}>
+                        <Button size="small" variant="contained" color="error">
+                          <DeleteForeverIcon
+                          onClick={()=> eliminarTarea(x.id)}
+                          ></DeleteForeverIcon>
+                        </Button>
+                      </Grid>
+                      {x.completado && (
+                        <Grid item lg={2}>
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="warning"
+                          >
+                            <EditIcon
+                              onClick={() => {
+                                setMostrarForm(true);
+                              }}
+                            ></EditIcon>
+                          </Button>
+                        </Grid>
+                      )}
                     </Grid>
                   </Grid>
                 </Grid>
               </Box>
-            );
-          })}
+              {mostrarForm && (
+                <TaskForm
+                  onCerrar={() => setMostrarForm(false)}
+                  tarea={x}
+                  editarTarea={editarTarea}
+                />
+              )}
+            </div>
+          ))}
         </div>
-      ) 
-    :
-    (
+      ) : (
         <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
           No tienes tareas pendientes!
         </Typography>
-      ) 
-      }
+      )}
     </>
   );
 }
