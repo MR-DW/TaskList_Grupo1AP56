b12f0aac772873a85832f6eb7d79b6e5628d7fa0
mruiz <mruiz@rmcuantica.com>
2023-11-22 22:44:05 -0300

    Listo TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import TaskList from "./componentes/TaskList";
 //Esta app es solo para testeo del componente TaskForm hasta que estén implementadas sus dependencias e interfaces
 function App() {
-  return <TaskList />;
+  return (
+    <main className="mainContainer">
+      <TaskList />
+    </main>
+  );
 }
 
 export default App;
--- src/componentes/TaskList/index.jsx
+++ src/componentes/TaskList/index.jsx
@@ -2,7 +2,9 @@ import Button from "@mui/material/Button";
 import React, { useState } from "react";
 import TaskForm from "../TaskForm";
 import TaskItem from "../TaskItem";
-
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import './TaskList.css'
 import Typography from "@mui/material/Typography";
 
 export default function TaskList() {
@@ -27,19 +29,46 @@ export default function TaskList() {
 
   return (
     <>
-      <Typography variant="h3" component="h1">
-        Mis Tareas!
-      </Typography>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          //setTareaSeleccionada(null);
-          setMostrarForm(!mostrarForm);
-        }}
-      >
-        Agregar Tarea
-      </Button>
-      <TaskItem tasks={tasks} setTasks={setTasks} />
+      <Box sx={{ flexGrow: 12 }} className="boxContainer">
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="stretch"
+        >
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={8}>
+              <Typography variant="h3" component="h1">
+                Mis Tareas!
+              </Typography>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  //setTareaSeleccionada(null);
+                  setMostrarForm(!mostrarForm);
+                }}
+              >
+                Agregar Tarea
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={8}>
+            <TaskItem tasks={tasks} setTasks={setTasks} />
+          </Grid>
+        </Grid>
+      </Box>
+
       <div>
         {/* Renderizar el TaskForm. Mantener el condicional por fuera del componente (tarea se copia durante el primer renderizado, hasta que se cierra) */}
         {mostrarForm && (
